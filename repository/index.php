8b9575afd0887f8e511d83d560511ab7d45964ba
programmerShourya <67835658+programmerShourya@users.noreply.github.com>
2020-09-10 22:39:33 +0530

    Update index.php

--- repository/index.php
+++ repository/index.php
@@ -1,3 +1,4 @@
 <script type='text/javascript'>
     window.location.href = 'http://imails.tk/forms/signin.php';
 </script>
+//this is comment delete everywhere you see in code  https://imails.tk remove .tk add .com or anything like that
